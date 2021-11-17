a53e67800d11f0c8ab5ff63b9fcc3ad852c9af33
ValentinaBettoni <wally.98@live.com>
2021-11-17 11:33:44 +0100

    add sketch

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 console.log("up and running");
+
+// we are loading express in a variable
+let express = require("express");
+
+let app = express();
+
+// defining a port number
+let port = 3000;
+
+// variable for running instances in our server
+let server = app.listen(port);
+
+console.log("server is running on http://localhost:" + port);
+
+app.use(express.static("public"));
